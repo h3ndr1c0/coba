5638e1208d288f19b1ef2717140d333c1dedf7fc
h3ndr1c0 <h3ndric0s@gmail.com>
2022-09-29 21:43:16 +0700

    coba1

---
+++ Tugas1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas1;
+
+import java.io.Console;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String Nama ="Hendrico Surya Saputra";
+        int Usia = 18;
+        
+        String JenisKelamin = "Laki Laki";
+        String TempatLahir = "Madiun";
+        int tgllahir = 10;
+        String blnlahir = "Januari";
+        int thnlahir = 2004;
+        
+        
+        System.out.println("Hallo");
+        System.out.println("Nama : " + Nama );
+        System.out.println("Usia : " + Usia );
+        System.out.println("JenisKelamin : " + JenisKelamin );
+        System.out.println("Tempat Lahir : " + TempatLahir);
+        System.out.println("Tanggal Lahir : " + tgllahir + " " + blnlahir + " "+ thnlahir);
+        
+
+        // TODO code application logic here
+    }
+    
+}
